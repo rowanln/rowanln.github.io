7cbc51c38ca015defc79dee58ff20dc8f4cc6eb0
rowanln <rowanln@gmail.com>
2016-06-12 15:23:38 -0700

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -1,6 +1,10 @@
-function addspace() {
-  document.getElementById("foo").style.paddingLeft="1"+document.getElementById("foo").style.paddingLeft;
+function changeColor(element) {
+  element.style.backgroundColor = randomColor():
 }
-<script src="/xjs/_/js/k=xjs.ntp.en_US.i4Rs5wjTYlI.O/m=spch/rt=j/d=0/t=zcms/rs=ACT90oEzrjDO1O2AoDZ9tpaoSddwG9PHaw" gapi_processed="true">
-</script>
 
+var colors = ["red", "green", "pink", "purple", "blue", "yellow", "orange"];
+
+function randomColor() {
+ var index = Math.floor(Math.random() * colors.length);
+ return colors[index];
+}
