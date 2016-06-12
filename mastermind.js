431bf19ea277c4a1a356d8c35ae9c8aa12bb48e0
rowanln <rowanln@gmail.com>
2016-06-12 14:45:33 -0700

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -1,4 +1,6 @@
 function addspace() {
   document.getElementById("foo").style.paddingLeft="1"+document.getElementById("foo").style.paddingLeft;
 }
+<script src="/xjs/_/js/k=xjs.ntp.en_US.i4Rs5wjTYlI.O/m=spch/rt=j/d=0/t=zcms/rs=ACT90oEzrjDO1O2AoDZ9tpaoSddwG9PHaw" gapi_processed="true">
+</script>
 
