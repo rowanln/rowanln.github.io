ddc871f2f893316a1b9ef67f0464ca60c61b7a49
rowanln <rowanln@gmail.com>
2016-07-05 14:47:50 -0700

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -2,9 +2,18 @@ function changeColor(element) {
   element.style.backgroundColor = randomColor();
 }
 
-var colors = ["red", "green", "pink", "purple", "blue", "yellow", "orange"];
+var colors = ["red", "green", "purple", "blue", "yellow", "orange"];
 
 function randomColor() {
  var index = Math.floor(Math.random() * colors.length);
  return colors[index];
+}
+function buildpalette() {
+  var palette = document.getElementById(palette);
+  var paletteColor = palette.children[0];
+  for(var i=0; i<colors.length; i++) {
+    var clone = paletteColor.cloneNode(true);
+    clone.style.backgroundColor = colors[i];
+    palette.appendChild(clone);
+  }
 }
