200ab47fc536601aa6c50bccad05c62029d83d30
rowanln <rowanln@gmail.com>
2016-07-05 14:52:29 -0700

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -8,12 +8,17 @@ function randomColor() {
  var index = Math.floor(Math.random() * colors.length);
  return colors[index];
 }
-function buildpalette() {
+
+function buildPalette() {
   var palette = document.getElementById(palette);
   var paletteColor = palette.children[0];
-  for(var i=0; i<colors.length; i++) {
+  for(var i = 0; i < colors.length; i++) {
     var clone = paletteColor.cloneNode(true);
     clone.style.backgroundColor = colors[i];
     palette.appendChild(clone);
   }
+}
+
+function buildPage() {
+  buildPalette();
 }
